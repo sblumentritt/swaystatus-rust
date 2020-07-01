b31bc231f62a5cd2ad38b45e5355af0adaa16c81
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-07-01 09:13:05 +0200

    Add run() function which contains the main loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::error::Error;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let mut count = 0;
+
+    loop {
+        println!("{}", count);
+        count += 1;
+        std::thread::sleep(Duration::from_secs(1));
+    }
 }
