88427b8a66a22f426b909b8ac8391663a962494a
Sebastian Blumentritt <blumentritt.sebastian@protonmail.com>
2020-07-06 10:47:25 +0200

    Add doc comments to traits/structs in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,39 @@
 use std::time::Duration;
 
+/// Functionality which a module needs to implement.
 pub trait Module {
+    /// Returns a string which will be used as is.
     fn value(&self) -> String;
+    /// Updates the internal state of the object.
     fn update(&mut self);
 }
 
+/// Holds a list of modules.
 pub struct Publisher {
     modules: Vec<Box<dyn Module>>,
 }
 
 impl Publisher {
+    /// Spacer which will be used between modules values.
     const SPACER: &'static str = " :: ";
 
+    /// Returns a publisher with an empty list.
     pub fn new() -> Self {
         Publisher {
             modules: Vec::new(),
         }
     }
 
+    /// Adds a module to the list
+    ///
+    /// # Arguments
+    ///
+    /// * `module` - An object which implements the Module trait.
     pub fn add(&mut self, module: Box<dyn Module>) {
         self.modules.push(module);
     }
 
+    /// Creates a string from all the modules and prints it on stdout.
     fn publish(&mut self) {
         let mut buffer = String::new();
 
@@ -39,6 +51,7 @@ impl Publisher {
         println!("{}", buffer);
     }
 
+    /// Main loop which publishes the data every 2 seconds.
     pub fn run(&mut self) {
         loop {
             self.publish();
